e3dd07bc9f75a7c9fcbaeead8f197e25e7007e12
Rico van Genugten <rico@rico-macbook.local>
2011-08-04 19:02:38 +0200

    wijzigingen Rob

--- hev_b.m
+++ hev_b.m
@@ -1,6 +1,9 @@
-function [X C I out] = hev(inp,par)
 
-global scale_em scale_eng
+
+function [X C I out] = hev_b(inp,par)
+
+
+global scale_em scale_eng n_s n_p m_v
 %function [X C I out] = hev(inp,par)
 %HEV Computes the resulting state-of-charge based on current state-
 %   of-charge, inputs and drive cycle demand.
@@ -17,22 +20,22 @@ global scale_em scale_eng
 
 
 % VEHICLE
-%   wheel radius            = 0.3 m
-%   rolling friction        = 144 N
-%   aerodynamic coefficient = 0.48 Ns^2/m^2
-%   vehicle mass            = 1800 kg
+%   wheel radius            = 0.28 m
+%   rolling friction        = 1.5/100 *vehicle mass*g N
+%   aerodynamic coefficient = 0.36 Ns^2/m^2
+%   vehicle mass            = m_v kg
 %
 % Wheel speed (rad/s)
-wv  = inp.W{1} ./ 0.3;
+wv  = inp.W{1} ./ 0.28;
 % Wheel acceleration (rad/s^2)
-dwv = inp.W{2} ./ 0.3;
+dwv = inp.W{2} ./ 0.28;
 % Wheel torque (Nm)
-Tv = (144 + 0.48.*inp.W{1}.^2 + 1800.*inp.W{2}) .* 0.3;
+Tv = ((m_v*0.015*9.81) + 0.36.*inp.W{1}.^2 + 1729.*inp.W{2}) .* 0.28;
 
 % TRANSMISSION
 %   gearbox efficiency = 0.95
 % gear ratios
-r_gear =  [17 9.6 6.3 4.6 3.7 3.5];
+r_gear =  [ 17.7305    6.4475    3.9401    2.8369    2.2163    1.8185];
 % Crankshaft speed (rad/s)
 wg  = (inp.W{3}>0) .* r_gear(inp.W{3} + (inp.W{3}==0)) .* wv;
 % Crankshaft acceleration (rad/s^2)
@@ -42,14 +45,18 @@ Tg  = (inp.W{3}>0) .* (Tv>0)  .* Tv ./ r_gear(inp.W{3} + (inp.W{3}==0)) ./ 0.95.
     + (inp.W{3}>0) .* (Tv<=0) .* Tv ./ r_gear(inp.W{3} + (inp.W{3}==0)) .* 0.95;
 
 % TORQUE SPLIT
-%   engine inertia = 0.14 m
+%   engine inertia = 0.1 m
 %   motor inertia  = 0.03 m
 %   idle speed     = 100 rad/s
-%
-Te0_list = [22.24   22.24   22.47   23.37   24.65   27.27   29.85   29.77]*scale_eng;
-we_list  = [112  168  224  280  336  392  447  503];
+
+fc_map_spd=[750:250:4500]*pi/30;
+fc_map_trq=[14.5 29 43.5 58 72.5 87 101.5 116 130.5 145 159.5 174 179.8];
+fc_max_trq=[6.18 8.24 9.09 10.24 11.57 12.24 12.42 12.3 12.24 12.24 12.12 12 11.6 11 10 8.55]*14.5;
+
+Te0_list = [20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20]*scale_eng;
+we_list  = fc_map_spd;
 % Engine drag torque (Nm)
-Te0  = dwg * 0.14 + interp1(we_list,Te0_list,min(max(wg,we_list(1)),we_list(end)));
+Te0  = dwg * 0.1 + interp1(we_list,Te0_list,min(max(wg,we_list(1)),we_list(end)));
 % Electric motor drag torque (Nm)
 Tm0  = dwg * 0.03;
 % Total required torque (Nm)
@@ -61,53 +68,83 @@ Tb  = (wg>0) .* (Ttot<=0) .* (1-inp.U{1}).*Ttot;
 Tm  = (wg>0) .*    inp.U{1} .*       Ttot;
 
 % ENGINE
-%   gasoline lower heating value = 42500000 J/kg
+% diesel lower heating value = 42426000 J/kg
+
+fc_fuel_map_gpkWh=[726	468	309	280	259	259	251	250	250	255	267	268	267
+613	370	289	265	249	241	236	235	235	237	239	240	240
+545	311	277	256	243	230	227	226	226	226	222	223	224
+500	305	279	253	241	230	225	222	219	218	218	217	215
+500	329	280	254	243	233	226	220	216	213	210	209	208
+495	331	286	259	245	234	227	221	215	210	207	204	203
+500	336	291	261	248	236	228	222	215	209	207	206	206
+500	339	297	264	248	238	230	224	218	215	209	208	208
+494	350	302	267	249	240	233	226	219	217	213	211	211
+534	369	314	273	250	243	237	230	226	221	217	217	217
+628	400	324	280	261	248	241	234	228	224	222	222	222
+832	460	336	293	270	250	245	237	230	227	227	227	227
+830	470	350	305	275	259	249	242	237	233	230	228	228
+905	500	365	317	280	266	254	247	242	238	234	234	234
+788	500	404	329	297	272	260	250	247	242	239	238	238
+1205	635	445	350	314	280	267	257	249	246	240	238	238];
 
 w_eng = wg;
 w_eng(w_eng<=100) = 100;
+w_eng(w_eng>=471) = 471;
 
 T_eng = Te;
 T_eng(T_eng<=15) = 15;
+% fuel consumption map in g/s
+[T,w]=meshgrid(fc_map_trq*scale_eng,fc_map_spd);
+fc_map_kW=T.*w/1000;
+fc_fuel_map=fc_fuel_map_gpkWh.*fc_map_kW/3600;
 % engine internal efficiency
-eta  = [0.423  0.420    0.446    0.445    0.446    0.445    0.440    0.423];
+% eta  = [0.423 0.4215 0.420 0.433 0.446 0.4455 0.445 0.4455 0.446 0.446 0.4455 0.445 0.4425 0.440 0.4315 0.423];
 % maximum engine 
-Tmax = [129  163  190  194  197  199  198  196]*scale_eng;
+Tmax = fc_max_trq*scale_eng;
+% fuel consumption per s
+e_gs = interp2(fc_map_trq*scale_eng,fc_map_spd,fc_fuel_map,Te,w_eng.*ones(size(Te)));
+e_gs(isnan(e_gs)) = 0;
 % Engine efficiency (function of speed)
-e_th = interp1(we_list,eta,w_eng,'linear*','extrap');
+% e_th = interp1(we_list,eta,w_eng,'linear*','extrap');
 % Fuel mass flow (function of power and efficiency)
-m_dot_fuel = Te.*w_eng./e_th./42500000;
+m_dot_fuel = e_gs./1000;
+% m_dot_fuel = Te.*w_eng./e_th./42426000;
 % Maximum engine torque
 Te_max = interp1(we_list,Tmax,w_eng,'linear*','extrap');
 % Fuel power consumption
-Pe = m_dot_fuel .* 42500000;
+Pe = m_dot_fuel .* 42426000;
 % Calculate infeasible
 ine = (Te > Te_max);
 
-
-% MOTOR (from ADVISOR PM25)
+% MOTOR (from ADVISOR PM32)
 % motor speed list
-wm_list = [0   50  100  150  200  250  300  350  400  450  500  550  600];
+red     = 0.5625;
+wm_list = [0 500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000]*(2*pi/60);
 % motor torque list
-Tm_list = [0 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160]*scale_em;
+Tm_list = [0 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200 210 220]*scale_em;
 % motor maximum torque (indexed by speed list)
-Tmmax   = [130 130 130 110 105 95 80 70 59 50 40 32 28]*scale_em;
+Tmmax   = 0.7111*[200 202 209 209 206 170 130 118 100 90 80 71 68 61 53 52 50]*scale_em;
 % motor minimum torque (indexed by speed list)
-Tmmin   = -Tmmax;
+Tmmin   = -1*0.7111*[200 202 209 209 206 170 130 118 100 90 80 71 68 61 53 52 50]*scale_em;
 % motor efficiency map (indexed by speed list and torque list)
-etam = 0.01*[...
-      50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50   
-      68 70 71 71 71 71 70 70 69 69 69 68 67 67 67 67 67  
-      68 75 80 81 81 81 81 81 81 81 80 80 79 78 77 76 76
-      68 77 81 85 85 85 85 85 85 84 84 83 83 82 82 80 79
-      68 78 82 87 88 88 88 88 88 87 87 86 86 85 84 83 83
-      68 78 82 88 88 89 89 89 88 88 87 85 85 84 84 84 83
-      69 78 83 87 88 89 89 88 87 85 85 84 84 84 84 84 83
-      69 73 82 86 87 88 87 86 85 84 84 84 84 84 84 84 83
-      69 71 80 83 85 86 85 85 84 84 84 84 84 84 83 83 83
-      69 69 79 82 84 84 84 84 83 83 83 83 83 83 83 82 82
-      69 68 75 81 82 81 81 81 81 81 81 80 80 80 80 80 80
-      69 68 73 80 81 80 76 76 76 76 76 76 76 76 75 75 75
-      69 68 71 75 75 75 75 75 75 75 75 75 74 74 74 74 74];
+etam    = 0.01*[...
+      70    75    78    77    73    72    70    68    66    65    60    59    58    55    52    50    49   48	47	 46	  45	44	  43
+      70    80    88    88.1  87    85.9  82    81    79    76    74    73    72    70    68    65    63   62	61	 60	  59	59.5  58	
+      70    80    88.1  89    88.5  88.2  87.6  86.1  84.4  82.8  82    80.7  80    79    76    75    73   72.5 72   71   70.2  70    69
+      70    80    88.1  90    90.1  90    89    88.2  87.9  86.8  86    85.3  84    82.7  82    81    80.2 80   79   78   76    75    74
+      70    80    88    90    92    92    90    89.6  88.7  88.3  88    87.2  86.5  86    85    84    83   82   81   80.9 80.6  80.3  80.2
+      70    80    88    89.5  90.2  92    91    90    89.5  89    88.7  88.2  87.7  87    86.5  86.1  85.9 84   83   82   82    81.9  81.8
+      70    80    88    89.8  90.3  91    92    91    90.1  89.7  89    88.5  88.2  87.7  87.2  86.8  86.2 86   85.5 85.4 85    85    85     
+      70    80    87.9  89.8  90.3  91    92    91    90.1  89.7  89.2  88.8  88.2  87.8  87.3  87    86.8 86.3 86   85.8 85    84    84
+      70    80    86.5  89    90    91.2  92    90.2  89.7  89.6  89    88.7  88.5  88    87.7  87    87   86.8 86.2 86   85.8  85.6  85
+      70    78    85    88    89    89.9  90.1  90    89.2  89    88.7  88.5  88    88    88    88    88   88	88	 88	  88	88	  88	
+      70    78    85    88    88.5  89    89.6  89.5  89.4  89    88.7  88.3  88.2  88    88    88    88   88	88	 88	  88	88	  88		
+      70    77    82    86.5  88.2  88.8  89    89    89    89    89    89    89    89    88    88    88   88	88	 88	  88	88	  88  	
+      70    76    82    86.1  88.1  88.5  88.8  89    89    89    89    89    89    89    89    88    88   88	88	 88	  88	88	  88
+      70	75	  81	86	  88	88.4  88.6	88.6  89	89	  89	89	  88	88	  88	88	  88   88	88	 88	  88	88	  88
+      70	74	  80.5  85.9  87.8	88.3  88.5	88.5  89	89	  89	89	  89	88	  88	88	  88   88	88	 88	  88	88	  88
+      70	73	  80.2  85.8  87.6	88.2  88.4	89	  89	89	  89	89	  89	89	  88	88	  88   88	88	 88	  88	88	  88
+   	  70	72.5  80    85.7  87	88.1  88.2	88.2  89	89	  89	89	  89	89	  88	88	  88   88	88	 88	  88	88	  88];
 % Electric motor efficiency
 e = (wg~=0) .* interp2(Tm_list,wm_list,etam,abs(Tm),wg.*ones(size(Tm))) + (wg==0);
 % Summarize infeasible
@@ -119,32 +156,32 @@ Pm =  (Tm<0) .* wg.*Tm.*e + (Tm>=0) .* wg.*Tm./e;
 
 % BATTERY
 % state-of-charge list
-soc_list = [0 0.2 0.4 0.6 0.8 1];
+soc_list = 0:0.1:1;
 % discharging resistance (indexed by state-of-charge list)
-R_dis    = [1.75    0.60    0.40    0.30    0.30    0.30]; % ohm
+R_dis    = [0.0377	0.0338	0.0300	0.0280	0.0275	0.0268	0.0269	0.0273	0.0283	0.0298	0.0312]*n_s/n_p; % ohm
 % charging resistance (indexed by state-of-charge list)
-R_chg    = [0.35    0.50    0.85    1.00    2.00    5.00]; % ohm
+R_chg    = [0.0235	0.0220	0.0205	0.0198	0.0198	0.0196	0.0198	0.0197	0.0203	0.0204	0.0204]*n_s/n_p; % ohm
 % open circuit voltage (indexed by state-of-charge list)
-V_oc     = [230     240     245     250     255     257]*scale_em; % volt
+V_oc     = [7.2370	7.4047	7.5106	7.5873	7.6459	7.6909	7.7294	7.7666	7.8078	7.9143	8.3645]*n_s*scale_em; % volt
 
 % Battery efficiency
-% columbic efficiency (0.9 when charging)
-e = (Pm>0) + (Pm<=0) .* 0.9;
+% columbic efficiency (0.9050 when charging)
+e = (Pm>0) + (Pm<=0) .* 0.9050;
 % Battery internal resistance
 r = (Pm>0)  .* interp1(soc_list, R_dis, inp.X{1},'linear*','extrap')...
   + (Pm<=0) .* interp1(soc_list, R_chg, inp.X{1},'linear*','extrap');
 
 % Battery current limitations
-%   battery capacity            = 6 Ah 
-%   maximum discharging current = 100A
-%   maximum charging current    = 125A
-im = (Pm>0) .* 100 + (Pm<=0) .* 125;
+%   battery capacity            = 6*n_p Ah 
+%   maximum discharging current = 63*n_p A
+%   maximum charging current    = 60*n_p A
+im = (Pm>0) .* 63*n_p + (Pm<=0) .* 60*n_p;
 % Battery voltage
 v = interp1(soc_list, V_oc, inp.X{1},'linear*','extrap');
 % Battery current
 Ib  =   e .* (v-sqrt(v.^2 - 4.*r.*Pm))./(2.*r);
 % New battery state of charge
-X{1}  = - Ib / (6 * 3600) + inp.X{1};
+X{1}  = - Ib / (6*n_p* 3600) + inp.X{1};
 % Battery power consumption
 Pb =   Ib .* v;
 % Update infeasible 
@@ -158,7 +195,7 @@ Ib   = (conj(Ib)+Ib)/2;
 % Summarize infeasible matrix
 I = (inb+ine+inm~=0);
 % Calculate cost matrix (fuel mass flow)
-C{1}  = (Pe / 42500000);
+C{1}  = (Pe / 42426000);
 
 % SIGNALS
 %   store relevant signals in out
--- hev_main_b.m
+++ hev_main_b.m
@@ -1,35 +1,56 @@
 % Start clean
 clear all; close all; clc
-global scale_em scale_eng
 
-load FTP_75
+% Set globals
+global scale_em scale_eng n_s n_p m_v
+
+% load models
+load FTP_75;run FC_CI67;
 speed_vector = V_z;
 acceleration_vector = [0;diff(V_z)];
 gearnumber_vector = G_z;
 
-Pmot = 20;
-Peng = 100;
+%% Scaling
 
-scale_eng = Peng/100;
-scale_em = Pmot/25;
+% Set motor powers
+Pmot = 61;
+Peng = 61;
+
+% Calculate scale factors
+scale_eng = Peng/67;
+scale_em = Pmot/32;
+
+% Set battery scaling
+n_s      = 43;
+n_p      = 4;
+
+% Set vehicle mass
+m_v      = 1729;
+
+
+%% Setup DP
 
 % create grid
-grd.Nx{1}    = 187; 
-grd.Xn{1}.hi = 0.7; 
-grd.Xn{1}.lo = 0.4;
+% state is SOC
+%grd.Nx{1}    = 187;   % Number of SOC grid points 
+grd.Nx{1} = 50;
+grd.Xn{1}.hi = 0.7;   % upper value for SOC
+grd.Xn{1}.lo = 0.4;   % lower value for SOC
 
-grd.Nu{1}    = 21; 
-grd.Un{1}.hi = 1; 
-grd.Un{1}.lo = -1;	% Att: Lower bound may vary with engine size.
+% input is electric motor utilization 
+% (-1 = max neg. power, 1 = max pos. power
+grd.Nu{1}    = 21;    % Number of input grid points
+grd.Un{1}.hi = 1;     % upper value for input
+grd.Un{1}.lo = -1;	  % lower value for input
 
-% set initial state
+% set initial SOC
 grd.X0{1} = 0.5;
 
-% final state constraints
+% final SOC constraints
 grd.XN{1}.hi = 0.51;
 grd.XN{1}.lo = 0.5;
 
-% define problem
+% define problem vectors to be used in problem function
 prb.W{1} = speed_vector; 
 prb.W{2} = acceleration_vector; 
 prb.W{3} = gearnumber_vector; 
@@ -38,8 +59,8 @@ prb.N  = 1876*1/prb.Ts + 1;
 
 % set options
 options = dpm();
-options.UseLine = 0;
-options.SaveMap = 1;
+options.UseLine = 1;
+options.SaveMap = 0;
 options.Iter = 5;
 options.InputType = 'd';
 options.FixedGrid = 0;
@@ -53,10 +74,16 @@ time = [0: 1877];
 % SOC
 figure
 plot(time,SOC)
+title('state of charge')
+xlabel('time [s]')
+ylabel('SOC [-]')
 
 % Fuel consumption
 figure
 plot(time(1:end-1),cumsum(res.m_dot_fuel))
+title('vehicle fuel consumption')
+xlabel('time [s]')
+ylabel('fuel used [kg]')
 
 % electric motor power vs drive power
 figure
@@ -70,10 +97,16 @@ title('Engine Map')
 xlabel('\omega [rad/s]')
 ylabel('Torque [Nm]')
 hold on
-we_list  = [112  168  224  280  336  392  447  503];
-Tmax = [129  163  190  194  197  199  198  196];
+ei = [0:5:230 230:10:260 260:20:300 300:50:400 400:100:1200];
+we_list  = [78.54 104.72 130.90 157.08 183.26 209.44 235.62 261.80 287.98 314.16 340.34 366.52 392.70 418.88 445.06 471.24];
+Tmax = [89.61 119.48 131.81 148.48 167.77 177.48 180.09 178.35 177.48 177.48 175.74 174 168.2 159.5 145 123.98];
 plot(we_list,Tmax*scale_eng,'Color',[0 0 0],'LineWidth',2) 
-%axis([100 510 0 210]);
+%axis([75 475 0 210]);
 hold on
 plot(res.w_eng,res.Te,'o')
+hold on
+[c,h] = contour(fc_map_spd,fc_map_trq*scale_eng,fc_fuel_map_gpkWh',ei);
+set(h,'ShowText','on','TextStep',get(h,'LevelStep')*2);
+fill([0,fc_map_spd fc_map_spd(end)],[max(fc_max_trq*scale_eng)+100 fc_max_trq*scale_eng max(fc_max_trq*scale_eng)+100],'w');
+axis ([min(fc_map_spd) max(fc_map_spd) 0 max(fc_max_trq)*scale_eng]);
 
